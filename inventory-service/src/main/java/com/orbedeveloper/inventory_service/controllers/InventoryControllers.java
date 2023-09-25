2caddc389c091f31bf40d65c209023ed3457df5e
orbe-developer <orbehin@gmail.com>
2023-09-25 23:08:25 +0100

    Fix errors (discovery server filters and version name)
    -Change the version name of the microservices endpoints
    -Fix the discovery server filters

--- inventory-service/src/main/java/com/orbedeveloper/inventory_service/controllers/InventoryControllers.java
+++ inventory-service/src/main/java/com/orbedeveloper/inventory_service/controllers/InventoryControllers.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/1/inventory")
+@RequestMapping("/api/v1/inventory")
 @RequiredArgsConstructor
 public class InventoryControllers {
     private final InventoryService inventoryService;
